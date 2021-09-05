b24d70e57c6ecb0a6bfb6632168694227145b3a2
Alexander Zharkov <alexander.06.zharkov@gmail.com>
2021-09-05 21:53:39 +0600

    Fix. Separated into functions expenses mapping and filtering

--- src/app/components/Expenses/Expenses.js
+++ src/app/components/Expenses/Expenses.js
@@ -6,6 +6,27 @@ import ExpenseFilter from '../Filters/ExpenseFilter';
 import { Grid } from '@material-ui/core';
 import { DATE_FILTERS } from '../../utils/constants';
 
+const mapExpenses = (expenses) => {
+  return expenses.map(({ title, amount, date }) => (
+    <ExpenseItem
+      title={title}
+      amount={amount}
+      date={date}
+      key={Math.random().toString()}
+    />
+  ));
+};
+
+const RenderedExpenses = ({ expenses, dateFilter }) => {
+  if (dateFilter === 'None') {
+    return mapExpenses(expenses);
+  }
+  const filteredExpenses = expenses.filter((value) => {
+    return dateFilter === value.date.getFullYear().toString();
+  });
+  return mapExpenses(filteredExpenses);
+};
+
 const Expenses = (props) => {
   const onFilterChange = (event) => {
     setDateFilter(event.target.value);
@@ -26,21 +47,7 @@ const Expenses = (props) => {
           onFilterChange={onFilterChange}
         />
         <Card className={css['expenses-list']}>
-          {props.expenses.map(({ title, amount, date }) => {
-            const component = (
-              <ExpenseItem
-                title={title}
-                amount={amount}
-                date={date}
-                key={Math.random().toString()}
-              />
-            );
-            if (dateFilter === 'None') return component;
-            if (dateFilter === new Date(date).getFullYear().toString()) {
-              return;
-            }
-            return null;
-          })}
+          <RenderedExpenses expenses={props.expenses} dateFilter={dateFilter} />
         </Card>
       </Grid>
     </Grid>
